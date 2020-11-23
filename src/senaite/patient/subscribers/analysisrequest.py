85169b3c972d95430eef2499b519f35346ec48f0
Ramon Bartl <rb@ridingbytes.com>
2020-11-23 10:19:59 +0100

    Refactored patient creation to own function

--- src/senaite/patient/subscribers/analysisrequest.py
+++ src/senaite/patient/subscribers/analysisrequest.py
@@ -7,17 +7,18 @@ from senaite.patient import logger
 def on_object_created(instance, event):
     """Event handler when a sample was created
     """
-    mrn = instance.getMedicalRecordNumberValue()
-    patient = patient_api.get_patient_by_mrn(mrn)
-    if patient is None:
-        logger.info("Creating new Patient with MRN #: {}".format(mrn))
-        values = get_patient_fields(instance)
-        patient_api.create_patient(mrn, **values)
+    ensure_patient(instance)
 
 
 def on_object_edited(instance, event):
     """Event handler when a sample was edited
     """
+    ensure_patient(instance)
+
+
+def ensure_patient(instance):
+    if instance.isMedicalRecordTemporary():
+        return
     mrn = instance.getMedicalRecordNumberValue()
     patient = patient_api.get_patient_by_mrn(mrn)
     if patient is None:
@@ -29,9 +30,9 @@ def on_object_edited(instance, event):
 def get_patient_fields(instance):
     """Extract the patient fields from the sample
     """
-    age = instance.getField("Age").get(instance)
     gender = instance.getField("Gender").get(instance)
     birthdate = instance.getField("DateOfBirth").get(instance)
+    age = instance.getField("Age").get(instance)
     address = instance.getField("PatientAddress").get(instance)
     fullname = instance.getField("PatientFullName").get(instance)
 
