15ebde08131a0bb47cc2030006eccefb9b5ca7b0
Ramon Bartl <rb@ridingbytes.com>
2022-10-20 22:57:01 +0200

    Patient sample add form

---
+++ src/senaite/patient/browser/patient/add2.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+from senaite.patient.api import get_patient_name_entry_mode
+from bika.lims.browser.analysisrequest.add2 import \
+    AnalysisRequestAddView as BaseView
+
+
+class PatientSampleAddView(BaseView):
+    """Patient specific sample add view
+    """
+    def __init__(self, context, request):
+        super(PatientSampleAddView, self).__init__(context, request)
+
+    def get_patient_address(self):
+        """Returns the patient address as a one-liner
+        """
+        records = self.context.getAddress()
+        for record in records:
+            address = record.get("address", "")
+            if address:
+                return address
+
+    def get_default_value(self, field, context, arnum):
+        """Get the default value of the field
+        """
+        name = field.getName()
+        mrn = self.context.getMRN()
+
+        # Inherit default values from the patient
+        if name == "MedicalRecordNumber":
+            if not mrn:
+                return {"temporary": True, "value": ""}
+            return {"temporary": False, "value": mrn}
+        elif name == "PatientID":
+            return self.context.getPatientID()
+        elif name == "PatientFullName":
+            entry_mode = get_patient_name_entry_mode()
+            if entry_mode == "parts":
+                return {
+                    "firstname": self.context.getFirstname(),
+                    "middlename": self.context.getMiddlename(),
+                    "lastname": self.context.getLastname(),
+                }
+            else:
+                return {"firstname": self.context.getFullname()}
+        elif name == "PatientAddress":
+            return self.get_patient_address()
+        elif name == "DateOfBirth":
+            birthdate = self.context.getBirthdate()
+            if birthdate:
+                return birthdate.strftime("%Y-%m-%d")
+        elif name == "Sex":
+            return self.context.getSex()
+        elif name == "Gender":
+            return self.context.getGender()
+
+        return super(PatientSampleAddView, self).get_default_value(
+            field, context, arnum)
