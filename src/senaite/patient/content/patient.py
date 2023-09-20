d992e7d74701df84b630b8e63289da4a59791cdd
Jordi Puiggené <jp@naralabs.com>
2023-09-20 11:50:46 +0200

    Remove pdb

--- src/senaite/patient/content/patient.py
+++ src/senaite/patient/content/patient.py
@@ -318,7 +318,6 @@ class IPatientSchema(model.Schema):
             # MRN is not a required field
             return
 
-        import pdb;pdb.set_trace()
         if not data.mrn:
             raise Invalid(_("Patient Medical Record is missing or empty"))
 
